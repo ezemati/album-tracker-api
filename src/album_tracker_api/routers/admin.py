2802a7a9b3a9ee81b377f7a2bf96dbdc42f6f0c6
Ezequiel Saidman <ezemati1904@gmail.com>
2026-05-10 19:28:27 -0300

    Add admin check to /admin routes

--- src/album_tracker_api/routers/admin.py
+++ src/album_tracker_api/routers/admin.py
@@ -1,12 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from ..core import AlbumTrackerSettings, settings
+from ..dependencies import get_current_admin_user
 from ..schemas import BaseResponse
 
-router = APIRouter(
-    prefix="/admin",
-    tags=["admin"],
-)
+router = APIRouter(prefix="/admin", tags=["admin"], dependencies=[Depends(get_current_admin_user)])
 
 
 @router.get("/settings")
