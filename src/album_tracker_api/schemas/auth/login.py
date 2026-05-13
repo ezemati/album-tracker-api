c99ce079a48c468b6cfa554894f9f198c5bc7a25
ezemati <40462592+ezemati@users.noreply.github.com>
2026-05-13 14:11:12 -0300

    Add User info to Login and Refresh (#4)

--- src/album_tracker_api/schemas/auth/login.py
+++ src/album_tracker_api/schemas/auth/login.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 
+from ..users import MeResponse
+
 
 # Use BaseModel and not BaseSchema since OAuth standard requires
 # snake_case fields
@@ -7,3 +9,4 @@ class LoginResponse(BaseModel):
     token_type: str = "bearer"
     access_token: str
     refresh_token: str
+    user: MeResponse
